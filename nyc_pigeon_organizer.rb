5cfe6c8d0bbbc82a4e6e8e68b1c4ddc59e202630
Rebecca Newborn <rjn58@cornell.edu>
2017-09-04 02:08:39 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  result = {}
+  data.each do |quality, qualhash| #eg. color, colorhash :color ==> [array of names]
+    qualhash.each do |attribute, namearray| #eg. :purple ==> [names] or "subway" ==> [names]
+      namearray.each do |name|
+        if result[name] == nil
+          result[name] = {color: [], gender: [], lives: []}
+          result[name][quality] << attribute.to_s
+        else
+          result[name][quality] << attribute.to_s
+        end
+      end
+    end
+  end
+  result
 end
